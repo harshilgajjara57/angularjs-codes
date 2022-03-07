8f111a2d39205d0a59fdae5205ee7fcb35ad1d76
harshil gajjar <harshilgajjara57@gmail.com>
2022-03-07 12:28:24 +0530

    extra directives added

--- directive/directiveScript.js
+++ directive/directiveScript.js
@@ -1,3 +1,32 @@
 // module of directives
 var myApp = angular.module("directives",[]);
 
+// controller for ngBind
+myApp.controller('ngBindController', ['$scope', function ($scope) {
+    
+    $scope.bindDemo = "default ngbind controller text";
+
+}]);
+
+// controller for ngChange
+myApp.controller('ngChangeController', ['$scope', function ($scope) {
+    
+    $scope.counter = 0;
+      $scope.change = function() {
+        $scope.counter++;
+      };
+
+}]);
+
+// controller for ngInit
+myApp.controller('ngInitController', ['$scope', function ($scope) {
+    
+    $scope.valueA = 0;
+    $scope.valueB;
+    $scope.sum = 0;
+
+    $scope.addVal = function() {
+        $scope.sum = $scope.valueA + $scope.valueB;
+    }
+
+}]);
